e6cfdb14a37f958efcf276bb46ce895b3091dd45
praveenmahasena647 <115216588+praveenmahasena647@users.noreply.github.com>
2023-12-26 22:03:39 -0500

    hard and leaking

--- cmd/model/exc.go
+++ cmd/model/exc.go
@@ -12,11 +12,62 @@ func (m *Model) Exc() error {
 	if err := m.projectInit(); err != nil {
 		return err
 	}
+	if err := m.getHTTPpackage(); err != nil {
+		return err
+	}
+	if err := m.getDBdriver(); err != nil {
+		return err
+	}
+	if err := m.generateTemplate(); err != nil {
+		return err
+	}
+	if err := m.gitInit(); err != nil {
+		return err
+	}
 	return nil
 }
 
 func (m *Model) projectInit() error {
 	return exec.Command("go", "mod", "init", m.ProjectName).Run()
+}
+
+func (m *Model) getHTTPpackage() error {
+	switch m.HTTPpackage {
+	case "Standered":
+		return nil
+	case "fastHTTP":
+		return exec.Command(
+			"go", "get", "-u", "github.com/qiangxue/fasthttp-routing",
+			"&&",
+			"go", "get", "-u", "github.com/valyala/fasthttp").Run()
+	}
+	return exec.Command("go", "get", "-u", m.HTTPpackage).Run()
+}
+
+func (m *Model) getDBdriver() error {
+	switch m.DBdriver {
+	case "gorm-postgres":
+		return exec.Command(
+			"go", "get", "-u", "gorm.io/gorm",
+			"&&",
+			"go", "get", "-u", "gorm.io/driver/postgres",
+		).Run()
+	case "gorm-MySQL":
+		return exec.Command(
+			"go", "get", "-u", "gorm.io/gorm",
+			"&&",
+			"go", "get", "-u", "gorm.io/driver/mysql",
+		).Run()
+	}
+	return exec.Command("go", "get", "-u", m.DBdriver).Run()
+}
+
+func (m *Model) generateTemplate() error {
+	return nil
+}
+
+func (m *Model) gitInit() error {
+	return exec.Command("git", "init").Run()
 }
 
 func chDir() error {
--- cmd/model/model.go
+++ cmd/model/model.go
@@ -3,7 +3,11 @@ package model
 import "github.com/charmbracelet/huh"
 
 type Model struct {
-	ProjectName string
+	ProjectName      string
+	HTTPpackage      string
+	DBdriver         string
+	GenerateTemplate bool
+	GitInit          bool
 }
 
 func New() *Model {
@@ -16,6 +20,44 @@ func (m *Model) Update() error {
 			huh.NewInput().
 				Title("pls Enter your project Name").
 				Value(&m.ProjectName),
+		),
+		huh.NewGroup(
+			huh.NewSelect[string]().
+				Title("Pls Select your HTTP lib || FrameWork").
+				Options(
+					huh.NewOption("Standered", "Standered"),
+					huh.NewOption("Gorlilla/Mux", "github.com/gorilla/mux"),
+					huh.NewOption("fastHTTP", "fastHTTP"),
+					huh.NewOption("go-Chi", "github.com/go-chi/chi/v5"),
+					huh.NewOption("go-Gin", "github.com/gin-gonic/gin"),
+					huh.NewOption("Echo", "github.com/labstack/echo/v4"),
+				).
+				Value(&m.HTTPpackage),
+		),
+		huh.NewGroup(
+			huh.NewSelect[string]().
+				Title("Pls Select your DB driver").
+				Options(
+					huh.NewOption("MongoDB", "go.mongodb.org/mongo-driver/mongo"),
+					huh.NewOption("gorm-postgres", "gorm-postgres"),
+					huh.NewOption("gorm-MySQL", "gorm-MySQL"),
+					huh.NewOption("Standered-postgresql", "github.com/lib/pq"),
+				).
+				Value(&m.HTTPpackage),
+		),
+		huh.NewGroup(
+			huh.NewConfirm().
+				Title("Generate Template").
+				Affirmative("Yes").
+				Negative("No").
+				Value(&m.GenerateTemplate),
+		),
+		huh.NewGroup(
+			huh.NewConfirm().
+				Title("Initiate Git").
+				Affirmative("Yes").
+				Negative("No").
+				Value(&m.GitInit),
 		),
 	)
 	return form.Run()
